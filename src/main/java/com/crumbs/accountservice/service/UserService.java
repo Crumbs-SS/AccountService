7df8d7da3380d7dfe1fda35be28bcee7c8ab50a9
JonathanFrey0000 <82041401+JonathanFrey0000@users.noreply.github.com>
2021-07-08 07:17:02 -0700

    added ownerExists, and return userId in register owner

--- src/main/java/com/crumbs/accountservice/service/UserService.java
+++ src/main/java/com/crumbs/accountservice/service/UserService.java
@@ -3,7 +3,10 @@ package com.crumbs.accountservice.service;
 import com.crumbs.lib.entity.UserDetails;
 import com.crumbs.lib.repository.UserDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -16,5 +19,13 @@ public class UserService {
 
     public UserDetails userById(int userId) {
         return userDetailsRepository.findById((long) userId).orElseThrow();
+    }
+
+    public Long ownerExists(String username){
+        UserDetails user = userDetailsRepository.findByUsername(username).orElseThrow();
+        if(user.getOwner()!= null)
+            return user.getId();
+        else
+            throw new NoSuchElementException();
     }
 }

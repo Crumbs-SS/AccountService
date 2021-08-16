ea7c025d0170af592460516fd6e5bab4c2ef7234
james.brower@smoothstack.com <james.brower@smoothstack.com>
2021-08-16 09:15:02 -0700

    Adjusted authentication code to check for user details status instead of just checking for presence of role object in user details. Added exception handling and response error messaging.

--- src/main/java/com/crumbs/accountservice/exception/ExceptionHelper.java
+++ src/main/java/com/crumbs/accountservice/exception/ExceptionHelper.java
@@ -1,5 +1,6 @@
 package com.crumbs.accountservice.exception;
 
+import lombok.extern.java.Log;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,10 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.NonUniqueResultException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -17,6 +22,17 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionHelper {
+
+    public void handleFilterExceptions(HttpServletRequest request, HttpServletResponse response, RuntimeException e) throws IOException {
+        if (e instanceof LoginException) {
+            LoginException(response);
+        }
+
+        if (e instanceof EmailNotConfirmedException) {
+            EmailNotConfirmedException(response);
+        }
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         Map<String, Object> body = new HashMap<>();
@@ -72,11 +88,23 @@ public class ExceptionHelper {
     }
 
     @ExceptionHandler(EmailNotConfirmedException.class)
-    public ResponseEntity<Object> EmailNotConfirmedException(){
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", "Your e-mail has not been confirmed yet. Please go to your inbox and click the confirmation link to activate your account.");
-        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    public void EmailNotConfirmedException(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("{\"timestamp\":\"" + LocalDateTime.now() + "\",\"message\":\"Your e-mail has not been confirmed yet. Please go to your inbox and click the confirmation link to activate your account.\"}");
+        out.flush();
+    }
+
+    @ExceptionHandler(LoginException.class)
+    public void LoginException(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("{\"timestamp\":\"" + LocalDateTime.now() + "\",\"message\":\"Username and password did not match an existing user.\"}");
+        out.flush();
     }
 
 }
